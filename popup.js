74cc2b3e6e3e9aff5edb78b072bdbbe4a78136df
Ulises Martinez <umtz08@gmail.com>
2017-11-12 12:01:04 -0800

    functional simple countdown timer added

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	document.getElementById("startButton").addEventListener("click", startTimer);
+	$("#startButton").on("click", startTimer);
 	$("#toggleTimer").click(function(){
         $("#timer").toggle();
     });
@@ -7,25 +7,42 @@ $(document).ready(function() {
 
  
 function startTimer() {
-	var hours = document.getElementById("hours");
-	var mins = document.getElementById("mins");
-	var secs = document.getElementById("secs");
 	
-	// will check for validity of numbers later
-	/*if (hours.value > 24 || hours.value < 0) {
-		hours.style.borderColor = "red";
-		isValid = false;
-	}
-	
-	if (mins.value > 60) {
-		mins.style.borderColor = "red";
-		isValid = false;
-	}
-	
-	if (secs.value > 60) {
-		secs.style.borderColor = "red";
-		isValid = false;
-	}*/
-	
-	
+	//taking user input
+    var time = {
+        hours: document.getElementById("hours").value * 1,
+        minutes: document.getElementById("mins").value * 1,
+        seconds: document.getElementById("secs").value * 1
+    };
+
+
+    // Save the interval's handle to `timer`
+    var timer = setInterval(countdown, 1000);
+
+    function countdown() {
+        var container = document.getElementById("timeLeft");
+
+        if (time.seconds > 0) {
+            time.seconds--;
+        }
+        else {
+            if (time.minutes > 0) {
+                time.minutes--;
+                time.seconds = 59;
+            }
+            else {
+                time.minutes = 59;
+                time.seconds = 59;
+                time.hours--;
+            }
+        }
+
+        if (time.hours >= 0) {
+            container.innerHTML = 'Please wait <b>' + time.hours + '</b> hours, <b>' + time.minutes + '</b> minutes, <b>' + time.seconds + '</b> seconds';
+        }
+        else {
+            container.innerHTML = 'Time over';
+            clearInterval(timer);
+        }
+    }
 }
