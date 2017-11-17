da2cf845821c959790553ea1f3f85a76d048c035
Ulises Martinez <umtz08@gmail.com>
2017-11-16 18:17:36 -0800

    moved timer to background.js

--- background.js
+++ background.js
@@ -1,7 +1,54 @@
-chrome.webRequest.onBeforeRequest.addListener(
+/*chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
 		return {redirectUrl: "www.yahoo.com"}; 
 	},
 	{urls: ["<all_urls>"]},
 	["blocking"]
-);
+);*/
+var interval;
+var timeout;
+var alarmDate;
+var alarmSound = new Audio("cuckoo.ogg");
+
+
+function setAlarm(hours, mins, secs) {
+	// set intveral variable in ms
+	interval = (hours * 3600000) + (mins * 60000) + (secs * 1000);  
+	clearTimeout(timeout);
+	
+	alarmDate = new Date();
+	alarmDate.setHours(alarmDate.getHours() + hours);
+	alarmDate.setMinutes(alarmDate.getMinutes() + mins);
+	alarmDate.setSeconds(alarmDate.getSeconds() + secs);
+	
+	timeout = setTimeout(ring, alarmDate.getTime() - new Date().getTime());
+	
+	//chrome.browserAction.setBadgeBackgroundColor({color:greenColor});
+    setInterval(function() {
+        chrome.browserAction.setBadgeText({text: getTimeLeftString()});
+    }, 1000);
+}
+
+function ring() {
+	var options = {
+		type: "basic",
+		title: "Primary Title",
+		message: "Primary message to display"/*,
+		iconUrl: "url_to_small_icon"*/
+	}
+	chrome.notifications.create("", options, null);
+	alarmSound.play();
+	shutAlarmOff();
+}
+
+function getTimeLeftString() {
+	
+}
+
+function shutAlarmOff() {
+	clearTimeout(timeout);
+    interval = 0;
+    alarmDate = null;
+    //pauseDate = null;
+    chrome.browserAction.setBadgeText({text: ""});
+}
--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+var bgpage = chrome.extension.getBackgroundPage();
+
 $(document).ready(function() {
 	$("#startButton").on("click", startTimer);
 	$("#toggleTimer").click(function(){
@@ -14,35 +16,6 @@ function startTimer() {
         minutes: document.getElementById("mins").value * 1,
         seconds: document.getElementById("secs").value * 1
     };
-
-
-    // Save the interval's handle to `timer`
-    var timer = setInterval(countdown, 1000);
-
-    function countdown() {
-        var container = document.getElementById("timeLeft");
-
-        if (time.seconds > 0) {
-            time.seconds--;
-        }
-        else {
-            if (time.minutes > 0) {
-                time.minutes--;
-                time.seconds = 59;
-            }
-            else {
-                time.minutes = 59;
-                time.seconds = 59;
-                time.hours--;
-            }
-        }
-
-        if (time.hours >= 0) {
-            container.innerHTML = 'Please wait <b>' + time.hours + '</b> hours, <b>' + time.minutes + '</b> minutes, <b>' + time.seconds + '</b> seconds';
-        }
-        else {
-            container.innerHTML = 'Time over';
-            clearInterval(timer);
-        }
-    }
+	
+	bgpage.setAlarm(time.hours, time.minutes, time.seconds);	
 }
