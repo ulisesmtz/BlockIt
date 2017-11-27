605a3607c7e16cdbb46e9cd9d54f5ad927fb076b
Ulises Martinez <umtz08@gmail.com>
2017-11-26 18:49:14 -0800

    fixed table with add/delete function

--- js/options.js
+++ js/options.js
@@ -1,41 +1,22 @@
 $(document).ready(function() {
-	document.getElementById("addItem").addEventListener("click", addItem);
-	document.getElementById("removeItem").addEventListener("click", removeItem);
-	// pressing enter wil also add site to list
-	$("#candidate").keyup(function(event) {
-		if (event.keyCode === 13) {
-			addItem();
-		}	
+	
+	$('#selectAll').click(function () {
+		$(this).closest("table").find("td input:checkbox").prop("checked", this.checked);
+	});
+	
+	$(".deleteSelected").click(function() {
+		$('input.checkRow:checkbox:checked').each(function(){
+			$(this).closest('tr').remove();
+			$("#selectAll").prop("checked", false);
+		});
+	});
+	
+	$("#table").on("click", ".deleteRow", function() {
+		$(this).closest('tr').remove();
+	});
+	
+	$(document).on("click", ".addRow", function() {
+		$('#table tr:last').after('<tr><td><input type="checkbox" class="checkRow"/></td><td>New</td><td><button value="Delete" class="deleteRow">Delete</button></td></tr>');
 	});
 
 });
-
-function addItem(){
-	var candidate = document.getElementById("candidate");
-	// if text is empty, return
-	if (!candidate.value.trim()) {
-		return;
-	}
-	
-	// if text does not start with www. add it automatically
-	if (!candidate.value.startsWith("www.")) {
-		candidate.value = "www." + candidate.value;	
-	}
-	var ul = document.getElementById("dynamic-list");
-	var li = document.createElement("li");
-	li.setAttribute('id',candidate.value);
-	li.appendChild(document.createTextNode(candidate.value));
-	ul.appendChild(li);
-	candidate.value = "";
-}
-
-function removeItem(){
-	var ul = document.getElementById("dynamic-list");
-	var candidate = document.getElementById("candidate");
-	var item = document.getElementById(candidate.value);
-	try {
-		ul.removeChild(item);
-	} catch (error) {
-		return false;
-	}
-}
